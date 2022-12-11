e54ff4424fd95d61efccdefd6795f4f9e7391cfb
johnhangen <68433301+johnhangen@users.noreply.github.com>
2022-12-11 15:33:52 -0500

    day 4 done

--- Day 4/main.py
+++ Day 4/main.py
@@ -27,7 +27,6 @@ def att1(input):
     spot0 = spots[0].split(sep='-')
     spot1 = spots[1].split(sep='-')
 
-      # (a_a <= b_a and a_b >= b_b) or (b_a <= a_a and b_b >= a_b)
     if (int(spot0[0]) <= int(spot1[0]) and int(spot0[1]) >= int(spot1[1])) or (int(spot1[0]) <= int(spot0[0]) and int(spot1[1]) >= int(spot0[1])):
       #print(f"{spot0[0]} <= {spot1[0]} and {spot1[1]} <= {spot0[1]})")
       count = count + 1
@@ -44,9 +43,11 @@ def att2(input):
     spot1 = spots[1].split(sep='-')
 
       # (a_a <= b_a and a_b >= b_b) or (b_a <= a_a and b_b >= a_b)
-    if int(spot0[0]) < int(spot1[0]) or int(spot0[1]) > int(spot1[1]) or int(spot1[0]) < int(spot0[0]) or int(spot1[1]) > int(spot0[1]):
-      #print(f"{spot0[0]} <= {spot1[0]} and {spot1[1]} <= {spot0[1]})")
+    if max(int(spot0[0]), int(spot1[0])) <= min(int(spot0[1]), int(spot1[1])):
       count = count + 1
+
+      # 780 too low
+      # 971 too high
 
   print(count)
 
